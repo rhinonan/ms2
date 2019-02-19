12a12bdccc0e3efc4d092a802a8acd71a7071b56
husan <rhinonan@163.com>
2019-02-19 20:25:50 +0800

    [add] 新增默认配置文件

---
+++ src/defaultConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    model: 'proxy',
+    target: null,
+    port: 3000,
+    https: false,
+    cacheMock: false,
+    cacheMockExtension: 'json',
+    mockDataPath: './__mock__',
+    staticPath: null
+};
