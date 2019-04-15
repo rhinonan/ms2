3c9640cb456efa62c7b0179d9298d7dff4e11bce
胡文超21859 <21859@sangfor.com>
2019-04-15 11:22:01 +0800

    [doc] 完善注释信息

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -1,15 +1,16 @@
 module.exports = {
-  model: 'proxy',
-  target: null,
-  port: 3000,
-  https: false,
-  changeOrigin: true,
-  secure: false,
-  cacheMock: false,
-  mockDataPath: './__mock__',
-  mockExtensions: ['json', 'html', 'xml'],
-  staticPath: null,
+  model: 'proxy', // 模式 proxy or mock
+  target: null, // 目标服务器地址
+  port: 3000, // 启动服务的端口，如果遇到端口占用。会按照规则分配另外的端口，请查看输出日志
+  https: false, // 同http-proxy-middleware
+  changeOrigin: true, // 同http-proxy-middleware
+  secure: false, // 同http-proxy-middleware
+  cacheMock: false, // 抓取服务器数据，只在proxy时生效
+  mockDataPath: './__mock__', // mock 文件目录
+  mockExtensions: ['json', 'html', 'xml'], // 自动抓取的文件类型
+  staticPath: null, // feature request
 
+  // url 到实际请求的映射
   handleMapPath(req) {
     // 剔除query
     const url = req.url.split('?')[0];
